9889a20f5acea9c95f96d760071ea7e625ba5273
DavydenkoAnton <davedenko83@gmail.com>
2019-01-09 19:58:17 +0300

    Create Main.java

---
+++ Kombinatorika/Main.java
@@ -0,0 +1,13 @@
+import Model.DNF;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Привет, комбинаторика !!!");
+        DNF maker=new DNF();
+        maker.initMatrix();
+        maker.transformMatrix();
+
+
+    }
+}
